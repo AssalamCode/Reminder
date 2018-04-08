7154300ae46e4ae858a27eb7ce81269ef7c64e05
adminkendol <chandra.comment@gmail.com>
2018-04-09 01:08:22 +0700

    Login Activity

--- app/src/main/java/com/tbreminder/source/MainActivity.java
+++ app/src/main/java/com/tbreminder/source/MainActivity.java
@@ -1,13 +1,50 @@
 package com.tbreminder.source;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.tbreminder.source.auth.Login;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnA;
+    Button btnB;
+    Button btnC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnA = (Button)findViewById(R.id.btnA);
+        btnB = (Button)findViewById(R.id.btnB);
+        btnC = (Button)findViewById(R.id.btnC);
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                intent.putExtra("type","Pasien");
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                intent.putExtra("type","Dokter");
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                intent.putExtra("type","Petugas");
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
